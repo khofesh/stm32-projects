33b0daac91545e9f90000e45e6f34d98ae44651b
fahmi ahmad <fahmiahmad@tutanota.com>
2025-06-21 19:18:20 +0700

    format the code using vscode

--- weact-stm32h5-64pin/002_PMS9003M/Core/Src/main.c
+++ weact-stm32h5-64pin/002_PMS9003M/Core/Src/main.c
@@ -60,12 +60,14 @@ static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 void air_sensor_power_test(void);
-void UART_Send(uint8_t *data, uint16_t length) {
+void UART_Send(uint8_t *data, uint16_t length)
+{
 	HAL_UART_Transmit(&huart2, data, length, HAL_MAX_DELAY);
 }
 
-void UART_SendString(char *str) {
-	HAL_UART_Transmit(&huart2, (uint8_t*) str, strlen(str), HAL_MAX_DELAY);
+void UART_SendString(char *str)
+{
+	HAL_UART_Transmit(&huart2, (uint8_t *)str, strlen(str), HAL_MAX_DELAY);
 }
 /* USER CODE END PFP */
 
@@ -78,7 +80,8 @@ void UART_SendString(char *str) {
  * @brief  The application entry point.
  * @retval int
  */
-int main(void) {
+int main(void)
+{
 
 	/* USER CODE BEGIN 1 */
 
@@ -108,12 +111,14 @@ int main(void) {
 	/* USER CODE BEGIN 2 */
 
 	if (!DFRobot_AirSensor_Init(&air_sensor, &hi2c3,
-			DFROBOT_AIR_SENSOR_DEFAULT_ADDR)) {
+								DFROBOT_AIR_SENSOR_DEFAULT_ADDR))
+	{
 		UART_SendString("failed to initialize the sensor\r\n");
 		return 1;
 	}
 
-	if (!DFRobot_AirSensor_Begin(&air_sensor)) {
+	if (!DFRobot_AirSensor_Begin(&air_sensor))
+	{
 		UART_SendString("failed to communicate with air quality sensor\r\n");
 		return 1;
 	}
@@ -130,23 +135,24 @@ int main(void) {
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-	while (1) {
+	while (1)
+	{
 		UART_SendString("\r\n=== Air Quality Measurements ===\r\n");
 
 		// read PM concentrations (µg/m³)
 		uint16_t pm1_0_std = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM1_0_STANDARD);
+			&air_sensor, PARTICLE_PM1_0_STANDARD);
 		uint16_t pm2_5_std = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM2_5_STANDARD);
+			&air_sensor, PARTICLE_PM2_5_STANDARD);
 		uint16_t pm10_std = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM10_STANDARD);
+			&air_sensor, PARTICLE_PM10_STANDARD);
 
 		uint16_t pm1_0_atm = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM1_0_ATMOSPHERE);
+			&air_sensor, PARTICLE_PM1_0_ATMOSPHERE);
 		uint16_t pm2_5_atm = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM2_5_ATMOSPHERE);
+			&air_sensor, PARTICLE_PM2_5_ATMOSPHERE);
 		uint16_t pm10_atm = DFRobot_AirSensor_GetParticleConcentration_ugm3(
-				&air_sensor, PARTICLE_PM10_ATMOSPHERE);
+			&air_sensor, PARTICLE_PM10_ATMOSPHERE);
 
 		UART_SendString("PM Concentrations (µg/m³):\r\n");
 		UART_SendString("  Standard conditions:\r\n");
@@ -174,17 +180,17 @@ int main(void) {
 
 		// read particle counts (per 0.1L air)
 		uint16_t count_0_3 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_0_3_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_0_3_UM_EVERY0_1L_AIR);
 		uint16_t count_0_5 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_0_5_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_0_5_UM_EVERY0_1L_AIR);
 		uint16_t count_1_0 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_1_0_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_1_0_UM_EVERY0_1L_AIR);
 		uint16_t count_2_5 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_2_5_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_2_5_UM_EVERY0_1L_AIR);
 		uint16_t count_5_0 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_5_0_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_5_0_UM_EVERY0_1L_AIR);
 		uint16_t count_10 = DFRobot_AirSensor_GetParticleNum_Every0_1L(
-				&air_sensor, PARTICLENUM_10_UM_EVERY0_1L_AIR);
+			&air_sensor, PARTICLENUM_10_UM_EVERY0_1L_AIR);
 
 		UART_SendString("\r\nParticle Counts (per 0.1L air):\r\n");
 
@@ -220,15 +226,17 @@ int main(void) {
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
 	/** Configure the main internal regulator output voltage
 	 */
 	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY))
+	{
 	}
 
 	/** Initializes the RCC Oscillators according to the specified parameters
@@ -239,21 +247,22 @@ void SystemClock_Config(void) {
 	RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV2;
 	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
 		Error_Handler();
 	}
 
 	/** Initializes the CPU, AHB and APB buses clocks
 	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK3;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK3;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 	RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+	{
 		Error_Handler();
 	}
 
@@ -267,7 +276,8 @@ void SystemClock_Config(void) {
  * @param None
  * @retval None
  */
-static void MX_I2C3_Init(void) {
+static void MX_I2C3_Init(void)
+{
 
 	/* USER CODE BEGIN I2C3_Init 0 */
 
@@ -285,26 +295,27 @@ static void MX_I2C3_Init(void) {
 	hi2c3.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
 	hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
 	hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	if (HAL_I2C_Init(&hi2c3) != HAL_OK) {
+	if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+	{
 		Error_Handler();
 	}
 
 	/** Configure Analogue filter
 	 */
-	if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE)
-			!= HAL_OK) {
+	if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+	{
 		Error_Handler();
 	}
 
 	/** Configure Digital filter
 	 */
-	if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK) {
+	if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
+	{
 		Error_Handler();
 	}
 	/* USER CODE BEGIN I2C3_Init 2 */
 
 	/* USER CODE END I2C3_Init 2 */
-
 }
 
 /**
@@ -312,7 +323,8 @@ static void MX_I2C3_Init(void) {
  * @param None
  * @retval None
  */
-static void MX_ICACHE_Init(void) {
+static void MX_ICACHE_Init(void)
+{
 
 	/* USER CODE BEGIN ICACHE_Init 0 */
 
@@ -324,16 +336,17 @@ static void MX_ICACHE_Init(void) {
 
 	/** Enable instruction cache in 1-way (direct mapped cache)
 	 */
-	if (HAL_ICACHE_ConfigAssociativityMode(ICACHE_1WAY) != HAL_OK) {
+	if (HAL_ICACHE_ConfigAssociativityMode(ICACHE_1WAY) != HAL_OK)
+	{
 		Error_Handler();
 	}
-	if (HAL_ICACHE_Enable() != HAL_OK) {
+	if (HAL_ICACHE_Enable() != HAL_OK)
+	{
 		Error_Handler();
 	}
 	/* USER CODE BEGIN ICACHE_Init 2 */
 
 	/* USER CODE END ICACHE_Init 2 */
-
 }
 
 /**
@@ -341,7 +354,8 @@ static void MX_ICACHE_Init(void) {
  * @param None
  * @retval None
  */
-static void MX_USART2_UART_Init(void) {
+static void MX_USART2_UART_Init(void)
+{
 
 	/* USER CODE BEGIN USART2_Init 0 */
 
@@ -361,24 +375,25 @@ static void MX_USART2_UART_Init(void) {
 	huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
 	huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
 	huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-	if (HAL_UART_Init(&huart2) != HAL_OK) {
+	if (HAL_UART_Init(&huart2) != HAL_OK)
+	{
 		Error_Handler();
 	}
-	if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8)
-			!= HAL_OK) {
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+	{
 		Error_Handler();
 	}
-	if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8)
-			!= HAL_OK) {
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+	{
 		Error_Handler();
 	}
-	if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK) {
+	if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
+	{
 		Error_Handler();
 	}
 	/* USER CODE BEGIN USART2_Init 2 */
 
 	/* USER CODE END USART2_Init 2 */
-
 }
 
 /**
@@ -386,8 +401,9 @@ static void MX_USART2_UART_Init(void) {
  * @param None
  * @retval None
  */
-static void MX_GPIO_Init(void) {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+static void MX_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
 	/* USER CODE BEGIN MX_GPIO_Init_1 */
 
 	/* USER CODE END MX_GPIO_Init_1 */
@@ -427,18 +443,21 @@ static void MX_GPIO_Init(void) {
 }
 
 /* USER CODE BEGIN 4 */
-void air_sensor_power_test(void) {
+void air_sensor_power_test(void)
+{
 	printf("\r\n=== Power Management Test ===\r\n");
 
 	// put sensor to sleep
-	if (DFRobot_AirSensor_SetLowPower(&air_sensor)) {
+	if (DFRobot_AirSensor_SetLowPower(&air_sensor))
+	{
 		printf("Sensor set to low power mode\r\n");
 	}
 
 	HAL_Delay(3000);
 
 	// wake up sensor
-	if (DFRobot_AirSensor_Awake(&air_sensor)) {
+	if (DFRobot_AirSensor_Awake(&air_sensor))
+	{
 		printf("Sensor awakened from low power mode\r\n");
 	}
 
@@ -451,28 +470,30 @@ void air_sensor_power_test(void) {
  * @brief  This function is executed in case of error occurrence.
  * @retval None
  */
-void Error_Handler(void) {
+void Error_Handler(void)
+{
 	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
-	while (1) {
+	while (1)
+	{
 	}
 	/* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
