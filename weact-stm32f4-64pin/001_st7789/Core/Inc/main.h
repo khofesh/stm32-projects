efade0912679a62c50af2a46a10cfeee44038848
fahmi ahmad <fahmiahmad@tutanota.com>
2025-07-05 12:57:40 +0700

    gpio_output for RST and DC

--- weact-stm32f4-64pin/001_st7789/Core/Inc/main.h
+++ weact-stm32f4-64pin/001_st7789/Core/Inc/main.h
@@ -57,6 +57,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LCD_DC_Pin GPIO_PIN_1
+#define LCD_DC_GPIO_Port GPIOA
+#define LCD_RST_Pin GPIO_PIN_2
+#define LCD_RST_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
---
+++ weact-stm32f4-64pin/001_st7789/README.md
@@ -0,0 +1,10 @@
+# ST7789
+
+SCL -> PA5 \
+SDA -> PA7 \
+CS -> PA4 \
+RST -> PA2 \
+DC -> PA1
+
+PA10 -> RX \
+PA9 -> TX
