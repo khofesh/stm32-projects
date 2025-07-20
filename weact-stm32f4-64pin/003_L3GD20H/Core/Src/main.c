65c5c9a384c7b8cc4b200f914c52d5683c430c6d
fahmi ahmad <fahmiahmad@tutanota.com>
2025-07-20 21:50:03 +0700

    setup pa1, pa2, and pa4

--- weact-stm32f4-64pin/003_L3GD20H/Core/Src/main.c
+++ weact-stm32f4-64pin/003_L3GD20H/Core/Src/main.c
@@ -227,12 +227,29 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA1 PA2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
--- weact-stm32f4-64pin/003_L3GD20H/README.md
+++ weact-stm32f4-64pin/003_L3GD20H/README.md
@@ -4,3 +4,16 @@ links:
 
 - https://github.com/STMicroelectronics/l3gd20h-pid
 - example: https://github.com/STMicroelectronics/STMems_Standard_C_drivers/blob/master/l3gd20h_STdC/README.md
+- https://www.adafruit.com/product/1032
+
+pin connection:
+
+- SCL -> PA5 (SCK)
+- SDA -> PA6 (MISO)
+- SA0 -> PA7 (MOSI)
+- CS -> PA4
+- DRDY -> PA1 (gpio input)
+- INT1 -> PA2 (gpio input)
+- GND
+- 3Vo
+- Vin -> none, leave unconnected (this is for input power if you want to power the board with 5V)
