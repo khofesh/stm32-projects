5728c2e4846c5b3273c7090d640232133129ac6f
fahmi ahmad <fahmiahmad@tutanota.com>
2025-08-16 17:25:40 +0700

    segger systemview integration

--- stm32f4disc/002_freertos_manual/Core/Src/main.c
+++ stm32f4disc/002_freertos_manual/Core/Src/main.c
@@ -34,6 +34,7 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define ever ;;
+#define DWT_CTRL (*(volatile uint32_t*)0xE0001000)
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -94,6 +95,13 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+
+  // enable the CYCCNT counter
+  DWT_CTRL |= (1 << 0);
+
+  SEGGER_SYSVIEW_Conf();
+
+  SEGGER_SYSVIEW_Start();
 
   status = xTaskCreate(task1_handler, "task-1", 200, "hello from task-1", 2, &task1_handle);
 
@@ -301,7 +309,7 @@ static void task1_handler(void *parameters)
 	for(ever)
 	{
 		printf("%s\n", (char*)parameters);
-		taskYIELD();
+//		taskYIELD();
 	}
 }
 
@@ -310,7 +318,7 @@ static void task2_handler(void *parameters)
 	for(ever)
 	{
 		printf("%s\n", (char*)parameters);
-		taskYIELD();
+//		taskYIELD();
 	}
 }
 /* USER CODE END 4 */
--- stm32f4disc/README.md
+++ stm32f4disc/README.md
@@ -8,3 +8,13 @@ https://www.st.com/en/evaluation-tools/stm32f4discovery.html
 - stm32f4 freertos config sample: https://github.com/FreeRTOS/FreeRTOS/blob/main/FreeRTOS/Demo/CORTEX_M4F_STM32F407ZG-SK/FreeRTOSConfig.h
 - API references: https://www.freertos.org/Documentation/02-Kernel/04-API-references/01-Task-creation/01-xTaskCreate
 
+### SystemView
+
+https://www.segger.com/downloads/systemview/
+
+get the following:
+
+- the SystemView application
+- SystemView Target Sources
+- SystemView User Manual
+
