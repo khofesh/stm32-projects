9435d9e9121ff3639a1f63291873bfd446136846
fahmi ahmad <fahmiahmad@tutanota.com>
2025-09-22 22:17:07 +0700

    set minSdk to 24

--- applications/SEN55Reader/app/build.gradle.kts
+++ applications/SEN55Reader/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.autonomousapps.dependency-analysis")
 }
 
 android {
@@ -10,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.fahimtu.sen55reader"
-        minSdk = 31
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- applications/SEN55Reader/build.gradle.kts
+++ applications/SEN55Reader/build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.autonomousapps.dependency-analysis") version "1.28.0"
+
 }
