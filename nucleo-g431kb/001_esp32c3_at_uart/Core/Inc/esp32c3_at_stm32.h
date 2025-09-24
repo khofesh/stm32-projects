25e512e3f6e8fb90fe57b531cd37416dd72fb505
fahmi ahmad <fahmiahmad@tutanota.com>
2025-09-24 10:56:54 +0700

    nucleo g432kb + esp32c3 with at firmware init

---
+++ nucleo-g431kb/001_esp32c3_at_uart/Core/Inc/esp32c3_at_stm32.h
@@ -0,0 +1,16 @@
+/*
+ * esp32c3_at_stm32.h
+ *
+ *  Created on: Sep 24, 2025
+ *      Author: fahmad
+ */
+
+#ifndef INC_ESP32C3_AT_STM32_H_
+#define INC_ESP32C3_AT_STM32_H_
+
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+
+#endif /* INC_ESP32C3_AT_STM32_H_ */
