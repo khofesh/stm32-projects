59d6ea01d297488c36718d584a2c7405957a4f03
fahmi ahmad <fahmiahmad@tutanota.com>
2025-10-05 20:42:11 +0700

    golang mqtt client done

--- applications/sensors-service/cmd/cli/mqtt-client/main.go
+++ applications/sensors-service/cmd/cli/mqtt-client/main.go
@@ -1,8 +1,66 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
 var broker string = "tcp://192.168.68.121:1883"
 var clientID string = "sensors-go-mqtt-client"
 
+var messageHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("received message on topic: %s\n", msg.Topic())
+	fmt.Printf("message: %s\n", msg.Payload())
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(c mqtt.Client, err error) {
+	fmt.Printf("Connection lost: %v\n", err)
+
+}
+
+var connectHandler mqtt.OnConnectHandler = func(c mqtt.Client) {
+	fmt.Println("connected to MQTT broker")
+}
+
 func main() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(broker)
+	opts.SetClientID(clientID)
+	opts.SetUsername("iot_device_user")
+	opts.SetPassword("___password___")
+	opts.SetDefaultPublishHandler(messageHandler)
+	opts.SetOnConnectHandler(connectHandler)
+	opts.SetConnectionLostHandler(connectLostHandler)
+	opts.SetAutoReconnect(true)
+	opts.SetKeepAlive(60 * time.Second)
+	opts.SetPingTimeout(10 * time.Second)
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to connect to MQTT broker: %v", token.Error())
+
+	}
+
+	topic := "sensors/bme280/all"
+	if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to subscribe to topic: %v", token.Error())
+	}
+	fmt.Printf("Subscribed to topic: %s\n", topic)
+
+	// wait for interrupt signal to gracefully shutdow
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+
+	// cleanup
+	fmt.Println("\nDisconnecting from MQTT broker...")
+	client.Disconnect(250)
+	fmt.Println("Disconnected")
 
 }
--- applications/sensors-service/go.mod
+++ applications/sensors-service/go.mod
@@ -3,14 +3,15 @@ module sensors-service
 go 1.25.0
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.1
 	github.com/lib/pq v1.10.9
 	tinygo.org/x/bluetooth v0.13.0
 )
 
 require (
-	github.com/eclipse/paho.golang v0.23.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/saltosystems/winrt-go v0.0.0-20240509164145-4f7860a3bd2b // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/soypat/cyw43439 v0.0.0-20250505012923-830110c8f4af // indirect
@@ -18,5 +19,7 @@ require (
 	github.com/tinygo-org/cbgo v0.0.4 // indirect
 	github.com/tinygo-org/pio v0.2.0 // indirect
 	golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 )
