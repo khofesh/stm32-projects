0cd08add17ce5ca6a486c33d409170b7f3bb1002
fahmi ahmad <fahmiahmad@tutanota.com>
2025-11-05 19:57:51 +0700

    nucleo stm32l432kc - dfrobot 10-dof init

---
+++ nucleo-l432kc/004_dfrobot-10dof/Drivers/ITG3200/itg3200.c
@@ -0,0 +1,8 @@
+/*
+ * itg3200.c
+ *
+ *  Created on: Nov 5, 2025
+ *      Author: fahmad
+ */
+
+#include "itg3200.h"
---
+++ nucleo-l432kc/004_dfrobot-10dof/Drivers/ITG3200/itg3200.h
@@ -0,0 +1,10 @@
+/*
+ * itg3200.h
+ *
+ *  Created on: Nov 5, 2025
+ *      Author: fahmad
+ */
+
+#pragma once
+
+
