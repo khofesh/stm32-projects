15c15d032129a98fb8f027c2fb2c9eaff3c0c697
fahmi ahmad <fahmiahmad@tutanota.com>
2025-11-11 11:38:09 +0700

    code completion ok

--- applications/sen55-serial-reader/main.cpp
+++ applications/sen55-serial-reader/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <
+#include <termios.h>
+
+struct Sen55Data
+{
+    float pm1_0, pm2_5, pm4_0, pm10;
+    float temperature, humidity;
+    int voc_index, nox_index;
+};
+
 
 int main()
 {
