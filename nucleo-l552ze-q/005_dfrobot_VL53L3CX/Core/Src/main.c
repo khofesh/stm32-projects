03ea731efc60610fe0f130af827fe446b6e41faa
fahmi ahmad <fahmiahmad@tutanota.com>
2025-12-20 00:08:57 +0700

    gpio interrupt

--- nucleo-l552ze-q/005_dfrobot_VL53L3CX/Core/Src/main.c
+++ nucleo-l552ze-q/005_dfrobot_VL53L3CX/Core/Src/main.c
@@ -330,7 +330,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA3 */
   GPIO_InitStruct.Pin = GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
