072c8a5f1bd9e20798c802e31e8d98190fd6f8db
fahmi ahmad <fahmiahmad@tutanota.com>
2025-12-19 22:31:03 +0700

    add ranging loop

--- nucleo-l552ze-q/005_dfrobot_VL53L3CX/Core/Src/main.c
+++ nucleo-l552ze-q/005_dfrobot_VL53L3CX/Core/Src/main.c
@@ -313,7 +313,77 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/* ranging and display loop */
+void RangingLoop(void)
+{
+ VL53LX_MultiRangingData_t MultiRangingData;
+ VL53LX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
+ uint8_t NewDataReady=0;
+ int no_of_object_found=0,j;
+ printf("Ranging loop starts\n");
 
+ status = VL53LX_WaitDeviceBooted(Dev);
+ status = VL53LX_DataInit(Dev);
+ status = VL53LX_StartMeasurement(Dev);
+
+ if(status){
+   printf("VL53LX_StartMeasurement failed: error = %d \n", status);
+   while(1);
+ }
+
+ if (isInterrupt){
+   do // HW interrupt mode
+   {
+     __WFI();
+     if(IntCount !=0 ){
+       IntCount=0;
+       status = VL53LX_GetMultiRangingData(Dev, pMultiRangingData);
+       no_of_object_found=pMultiRangingData->NumberOfObjectsFound;
+       printf("Count=%5d, ", pMultiRangingData->StreamCount);
+       printf("#Objs=%1d ", no_of_object_found);
+       for(j=0;j<no_of_object_found;j++){
+         if(j!=0)printf("\n                     ");
+         printf("status=%d, D=%5dmm, Signal=%2.2f Mcps, Ambient=%2.2f Mcps",
+                pMultiRangingData->RangeData[j].RangeStatus,
+                pMultiRangingData->RangeData[j].RangeMilliMeter,
+                pMultiRangingData->RangeData[j].SignalRateRtnMegaCps/65536.0,
+                pMultiRangingData->RangeData[j].AmbientRateRtnMegaCps/65536.0);
+       }
+       printf ("\n");
+       if (status==0){
+         status = VL53LX_ClearInterruptAndStartMeasurement(Dev);
+       }
+
+     }
+   }
+   while(1);
+ }
+ else{
+   do{ // polling mode
+     status = VL53LX_GetMeasurementDataReady(Dev, &NewDataReady);
+     HAL_Delay(1); // 1 ms polling period, could be longer.
+     if((!status)&&(NewDataReady!=0)){
+       status = VL53LX_GetMultiRangingData(Dev, pMultiRangingData);
+       no_of_object_found=pMultiRangingData->NumberOfObjectsFound;
+       printf("Count=%5d, ", pMultiRangingData->StreamCount);
+       printf("#Objs=%1d ", no_of_object_found);
+       for(j=0;j<no_of_object_found;j++){
+         if(j!=0)printf("\n                     ");
+         printf("status=%d, D=%5dmm, Signal=%2.2f Mcps, Ambient=%2.2f Mcps",
+                pMultiRangingData->RangeData[j].RangeStatus,
+                pMultiRangingData->RangeData[j].RangeMilliMeter,
+                pMultiRangingData->RangeData[j].SignalRateRtnMegaCps/65536.0,
+                pMultiRangingData->RangeData[j].AmbientRateRtnMegaCps/65536.0);
+       }
+       printf ("\n");
+       if (status==0){
+         status = VL53LX_ClearInterruptAndStartMeasurement(Dev);
+       }
+     }
+   }
+   while (1);
+ }
+}
 /* USER CODE END 4 */
 
 /**
