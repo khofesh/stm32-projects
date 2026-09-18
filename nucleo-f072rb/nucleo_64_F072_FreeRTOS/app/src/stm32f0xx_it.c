451882918cba5d37e4b05926c4f481cbc8393a02
fahmi ahmad <fahmiahmad@tutanota.com>
2026-09-18 21:44:25 +0700

    Interrupt with context switching

--- nucleo-f072rb/nucleo_64_F072_FreeRTOS/app/src/stm32f0xx_it.c
+++ nucleo-f072rb/nucleo_64_F072_FreeRTOS/app/src/stm32f0xx_it.c
@@ -105,6 +105,8 @@ void HardFault_Handler(void)
   */
 void EXTI4_15_IRQHandler()
 {
+	portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+
 	// Test for line 13 pending interrupt
 	if ((EXTI->PR & EXTI_PR_PR13_Msk) != 0)
 	{
@@ -112,7 +114,10 @@ void EXTI4_15_IRQHandler()
 		EXTI->PR = EXTI_PR_PR13;
 
 		// Release the semaphore
-		xSemaphoreGiveFromISR(xSem, NULL);
+		xSemaphoreGiveFromISR(xSem, &xHigherPriorityTaskWoken);
+
+	    // Perform a context switch to the waiting task
+	    portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
 	}
 }
 
